851b0720d1ad17ae916553bb75827497a1b4fb3d
Garrett Hu <garretthu4@gmail.com>
2022-03-17 12:26:07 -0700

    added cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
@@ -8,6 +9,7 @@ const reviewsRouter = require("./reviews/reviews.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
+app.use(cors());
 app.use(express.json());
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
